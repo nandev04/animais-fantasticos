3ca616f198af0289fa6743dc50afc863b53e0b77
Renan Alves <renanlvsdv@gmail.com>
2023-09-05 04:04:12 -0300

    TabNav refatorado

--- js/modules/navegacaoTab.js
+++ js/modules/navegacaoTab.js
@@ -2,25 +2,30 @@ export default class TabNav {
   constructor(listaMenu, listaConteudo) {
   this.menu = document.querySelectorAll(listaMenu);
   this.content = document.querySelectorAll(listaConteudo);
+  this.activeClass = 'ativo';
   }
 
+  // Ativa a tab de acordo com o index
   activeTab(index) {
     this.content.forEach((item) => {
-      item.classList.remove('ativo');
+      item.classList.remove(this.activeClass);
     });
     const direcao = this.content[index].dataset.anime;
-    this.content[index].classList.add('ativo', direcao);
+    this.content[index].classList.add(this.activeClass, direcao);
+  }
+
+  //Adiciona os eventos nas tabs
+  addTabEvent() {
+    this.menu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => this.activeTab(index));
+    });
   }
 
   init() {
     if (this.content.length && this.menu.length) {
-      this.content[0].classList.add('ativo');
-  
-      this.menu.forEach((itemMenu, index) => {
-        itemMenu.addEventListener('click', () => {
-          this.activeTab(index);
-        });
-      });
+      //Ativar primeiro item
+      this.activeTab(0);
+      this.addTabEvent()
     }
   }
 }
